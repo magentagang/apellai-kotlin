90d8e66e1fa3193d2b4e9f9bb59732b9079ecf1b
Metalicana <alradi9923@gmail.com>
2021-05-25 15:43:22 +0600

    Added a Loggin application called Timber. Used it to track loggin of activity lifecycles.

---
+++ app/src/main/java/com/magentagang/apellai/LoggingApp.kt
@@ -0,0 +1,11 @@
+package com.magentagang.apellai
+
+import android.app.Application
+import timber.log.Timber
+
+class LoggingApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
--- app/src/main/java/com/magentagang/apellai/MainActivity.kt
+++ app/src/main/java/com/magentagang/apellai/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Timber.i("on Create was invoked")
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
@@ -23,5 +25,10 @@ class MainActivity : AppCompatActivity() {
             R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.i("on Start was invoked")
     }
 }
