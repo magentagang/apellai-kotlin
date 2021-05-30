2a78cc6d5940e57d16f0e15f0de06cb5a5528f4e
arg274 <deeparghya.csedu@gmail.com>
2021-05-30 17:27:25 +0600

    Fix some margins

--- app/src/main/java/com/magentagang/apellai/ui/fragments/CardAlbumHScroll.kt
+++ app/src/main/java/com/magentagang/apellai/ui/fragments/CardAlbumHScroll.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.magentagang.apellai.R
 import com.magentagang.apellai.adapter.CardAlbumHScrollAdapter
