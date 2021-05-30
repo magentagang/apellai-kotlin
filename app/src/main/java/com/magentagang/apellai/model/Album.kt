52b5d8a3b73dd797f2b1c881c76125b3215128d7
arg274 <deeparghya.csedu@gmail.com>
2021-05-31 03:04:39 +0600

    Add Spinner placeholder + Fix margins

--- app/src/main/java/com/magentagang/apellai/model/Album.kt
+++ app/src/main/java/com/magentagang/apellai/model/Album.kt
@@ -2,7 +2,7 @@ package com.magentagang.apellai.model
 
 data class Album (
     var ID: String = "",
-    var name: String = "Extremely Long Title Because Artists Like to Mess Around For No Reason",
-    var artist: String = "Extremely Long Artist with 120 Featured Artists Because the Main Artist is A Talentless Hack",
+    var name: String = "Kalk Samen Kuri no Hana",
+    var artist: String = "Sheena Ringo",
     var year: Int = 2069,
 )
--- app/src/main/java/com/magentagang/apellai/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/magentagang/apellai/ui/library/LibraryFragment.kt
@@ -4,27 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.magentagang.apellai.R
 
 class LibraryFragment : Fragment() {
 
-  private lateinit var libraryViewModel: LibraryViewModel
+    private lateinit var libraryViewModel: LibraryViewModel
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    libraryViewModel =
-            ViewModelProvider(this).get(LibraryViewModel::class.java)
-    val root = inflater.inflate(R.layout.fragment_library, container, false)
-    val textView: TextView = root.findViewById(R.id.text_library)
-    libraryViewModel.text.observe(viewLifecycleOwner, {
-      textView.text = it
-    })
-    return root
-  }
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        libraryViewModel =
+                ViewModelProvider(this).get(LibraryViewModel::class.java)
+        return inflater.inflate(R.layout.fragment_library, container, false)
+    }
 }
