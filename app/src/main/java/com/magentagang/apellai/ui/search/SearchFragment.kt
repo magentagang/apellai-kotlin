206b82f3c571e2259002a5693e3f410b1b067873
arg274 <deeparghya.csedu@gmail.com>
2021-05-31 03:30:15 +0600

    Add search placeholder

--- app/src/main/java/com/magentagang/apellai/ui/search/SearchFragment.kt
+++ app/src/main/java/com/magentagang/apellai/ui/search/SearchFragment.kt
@@ -4,27 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.magentagang.apellai.R
 
 class SearchFragment : Fragment() {
 
-  private lateinit var searchViewModel: SearchViewModel
+    private lateinit var searchViewModel: SearchViewModel
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    searchViewModel =
-            ViewModelProvider(this).get(SearchViewModel::class.java)
-    val root = inflater.inflate(R.layout.fragment_search, container, false)
-    val textView: TextView = root.findViewById(R.id.text_search)
-    searchViewModel.text.observe(viewLifecycleOwner, {
-      textView.text = it
-    })
-    return root
-  }
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        searchViewModel =
+                ViewModelProvider(this).get(SearchViewModel::class.java)
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
 }
