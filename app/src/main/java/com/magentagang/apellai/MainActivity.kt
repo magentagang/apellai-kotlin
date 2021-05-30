42d2d08831d2897cbd2078121987e45efa371bdf
arg274 <deeparghya.csedu@gmail.com>
2021-05-30 14:44:19 +0600

    Populate the home fragment + Font override + Themeing changes

--- app/src/main/java/com/magentagang/apellai/MainActivity.kt
+++ app/src/main/java/com/magentagang/apellai/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         // peepeepooopoo radi branch
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.navigation_home, R.id.navigation_search, R.id.navigation_library))
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
