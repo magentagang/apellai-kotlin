774979a7071c211e0adde13f697262565e74a49b
Metalicana <alradi9923@gmail.com>
2021-06-21 01:25:53 +0600

    completed the whole searching mechanics

--- app/src/main/java/com/magentagang/apellai/ui/search/SearchFragment.kt
+++ app/src/main/java/com/magentagang/apellai/ui/search/SearchFragment.kt
@@ -5,12 +5,21 @@ import android.content.Context
 import android.os.Bundle
 import android.view.*
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.magentagang.apellai.R
+import com.magentagang.apellai.adapter.*
 import com.magentagang.apellai.databinding.FragmentSearchBinding
+import com.magentagang.apellai.model.Album
+import com.magentagang.apellai.model.Artist
+import com.magentagang.apellai.ui.artistscreen.ArtistScreenDirections
+import com.magentagang.apellai.ui.library.LibraryFragmentDirections
 import com.magentagang.apellai.viewmodel.CardAlbumHScrollViewModel
 import com.magentagang.apellai.viewmodel.factory.CardAlbumHScrollViewModelFactory
 import timber.log.Timber
@@ -30,27 +39,84 @@ class SearchFragment : Fragment() {
         val searchViewModel = ViewModelProvider(this, viewModelFactory).get(
             SearchViewModel::class.java)
 
+
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_search, container, false
         )
-        binding.chipAlbum.setOnClickListener {
-        }
         binding.searchViewModel = searchViewModel
         binding.lifecycleOwner = this
+
+        val adapterAlbum = ListAlbumAdapter(AlbumListener { ID ->
+            searchViewModel.onAlbumClicked(ID)
+        })
+        val adapterArtist = ListArtistAdapter(ArtistListener { ID ->
+            searchViewModel.onArtistClicked(ID)
+        })
+        val adapterTrack = ListTrackAdapter(TrackListener { ID ->
+            searchViewModel.onArtistClicked(ID)
+        })
+
+
+        binding.searchResult.adapter = adapterAlbum
+
+        searchViewModel.albums.observe(viewLifecycleOwner, {
+            it?.let {
+                adapterAlbum.submitList(it)
+            }
+        })
+        searchViewModel.artists.observe(viewLifecycleOwner, {
+            it?.let {
+                adapterArtist.submitList(it)
+            }
+        })
+
+        searchViewModel.navigateToAlbumScreen.observe(viewLifecycleOwner, { id ->
+            id?.let {
+                val navController = this.findNavController()
+                navController.navigate(SearchFragmentDirections.actionNavigationSearchToAlbumScreen(id))
+                searchViewModel.doneNavigating()
+            }
+        })
+
         searchViewModel.subsonicResponseRoot.observe(viewLifecycleOwner, {
             //Timber.i("ALBUMS SEARCH -> ${it.toString()}")
             if(it != null){
                 Timber.i("Search Result -> ${it.subsonicResponse.searchResult3?.album.toString() ?: "searchResult3 is null"}")
+                val chipChecked = binding.chipGroup.checkedChipId
+                adapterAlbum.submitList(it.subsonicResponse.searchResult3?.album)
+                adapterArtist.submitList(it.subsonicResponse.searchResult3?.artist)
+                adapterTrack.submitList(it.subsonicResponse.searchResult3?.song)
+                //TODO BUTUM HALA ETAR NAM SONG DISOS KEN TRACK HOBE
+                when(chipChecked)
+                {
+                    binding.chipAlbum.id -> adapterAlbum.notifyDataSetChanged()
+                    binding.chipArtist.id -> adapterArtist.notifyDataSetChanged()
+                    binding.chipTrack.id -> adapterTrack.notifyDataSetChanged()
+                }
+
             }else{
                 Timber.i("Search Result -> subsonicResponseRoot is null")
             }
         })
 
+        binding.chipAlbum.setOnClickListener {
+            binding.searchResult.adapter = adapterAlbum
+            adapterAlbum.notifyDataSetChanged()
+        }
+        binding.chipArtist.setOnClickListener {
+            binding.searchResult.adapter = adapterArtist
+            adapterArtist.notifyDataSetChanged()
+
+        }
+        binding.chipTrack.setOnClickListener {
+            binding.searchResult.adapter = adapterTrack
+            adapterTrack.notifyDataSetChanged()
+        }
+
         binding.searchView.onQueryTextChanged {
             Timber.i("Search View Text: $it")
             searchViewModel.searchQuery.value = it
         }
-
         return binding.root
     }
 
--- app/src/main/java/com/magentagang/apellai/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/magentagang/apellai/ui/search/SearchViewModel.kt
@@ -3,10 +3,15 @@ package com.magentagang.apellai.ui.search
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.asLiveData
+import com.magentagang.apellai.model.Album
+import com.magentagang.apellai.model.Artist
 import com.magentagang.apellai.model.SubsonicResponseRoot
+import com.magentagang.apellai.model.Track
 import com.magentagang.apellai.repository.database.DatabaseDao
 import com.magentagang.apellai.repository.database.UserDatabase
+import com.magentagang.apellai.util.Constants
 import com.magentagang.apellai.util.RepositoryUtils
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,17 +21,50 @@ import kotlinx.coroutines.flow.flatMapLatest
 class SearchViewModel(application: Application) : AndroidViewModel(application) {
     var databaseDao: DatabaseDao = UserDatabase.getInstance(application).databaseDao()
     val searchQuery = MutableStateFlow("")
-    var repositoryUtils: RepositoryUtils
+    var repositoryUtils: RepositoryUtils = RepositoryUtils(databaseDao)
+    var albums = MutableLiveData<List<Album>>()
+    var artists = MutableLiveData<List<Artist>>()
+    var tracks = MutableLiveData<List<Track>>()
     var subsonicResponseRoot:LiveData<SubsonicResponseRoot>
     init{
-        repositoryUtils = RepositoryUtils(databaseDao)
 
         val subsonicResponseRootFlow = searchQuery.flatMapLatest {
             repositoryUtils.fetchSearchResultFlow(it)
         }
+        repositoryUtils.retrieveAllAlbums(Constants.TYPE_ALPHABETICAL_BY_NAME)
 
         subsonicResponseRoot = subsonicResponseRootFlow.asLiveData()
     }
+
+    private val _navigateToAlbumScreen: MutableLiveData<String?> = MutableLiveData<String?>()
+    val navigateToAlbumScreen
+        get() = _navigateToAlbumScreen
+
+    private val _navigateToArtistScreen = MutableLiveData<String?>()
+    val navigateToArtistScreen
+        get() = _navigateToArtistScreen
+
+    private val _navigateToNowPlayingScreen = MutableLiveData<String?>()
+    val navigateToNowPlayingScreen
+        get() = _navigateToNowPlayingScreen
+
+    fun onTrackClicked(id: String){
+        _navigateToNowPlayingScreen.value = id
+    }
+
+    fun onAlbumClicked(id: String) {
+        _navigateToAlbumScreen.value = id
+    }
+    fun onArtistClicked(id: String){
+        _navigateToArtistScreen.value = id
+    }
+    fun doneNavigating() {
+        _navigateToAlbumScreen.value = null
+        _navigateToArtistScreen.value = null
+        _navigateToNowPlayingScreen.value = null
+    }
+
+
 
 
 }
