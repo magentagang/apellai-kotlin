f2af1ac79123bf10fa82861bfc19652ed75e57c0
Jahir Sadik <59853499+jahirsadik@users.noreply.github.com>
2021-06-22 12:38:56 +0600

    Created new login implementation branch.

--- app/src/main/java/com/magentagang/apellai/ui/login/LoginScreenFragment.kt
+++ app/src/main/java/com/magentagang/apellai/ui/login/LoginScreenFragment.kt
@@ -1,5 +1,6 @@
 package com.magentagang.apellai.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,11 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.magentagang.apellai.MainActivity
 import com.magentagang.apellai.R
 import com.magentagang.apellai.databinding.FragmentLoginScreenBinding
+import timber.log.Timber
+
 
 class LoginScreenFragment : Fragment() {
     private lateinit var loginScreenViewModel: LoginScreenViewModel
@@ -29,6 +33,7 @@ class LoginScreenFragment : Fragment() {
         )
         binding.loginScreenViewModel = loginScreenViewModel
         binding.lifecycleOwner = this
+        Toast.makeText(application,"Logging Screen Created Successfully",Toast.LENGTH_LONG).show()
 
         // TODO(Put authentication logic here)
         /*
@@ -37,6 +42,7 @@ class LoginScreenFragment : Fragment() {
          */
         binding.loginButton.setOnClickListener {
             // FIXME(Even though server address is sent, logic is implemented for one server only)
+            Timber.i("Login Button Clicked: ${binding.serverAddress.text}, ${binding.username.text}, ${binding.password.text}")
             loginScreenViewModel.storeCredentialsOrToast(binding.serverAddress, binding.username, binding.password)
         }
 
@@ -45,7 +51,8 @@ class LoginScreenFragment : Fragment() {
                 if (it == true) {
                     Toast.makeText(application,"Logged in successfully",Toast.LENGTH_LONG).show()
                     //TODO(Just navigate from here to home screen safely)
-
+                    val mainIntent = Intent(activity, MainActivity::class.java)
+                    startActivity(mainIntent)
                     loginScreenViewModel.setNavigateBooleanToNull()
                 }else if(!it){
                     Toast.makeText(application,"Incorrect Credentials",Toast.LENGTH_LONG).show()
